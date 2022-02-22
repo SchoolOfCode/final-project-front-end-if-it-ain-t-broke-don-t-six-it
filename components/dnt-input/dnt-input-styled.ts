ff7adf01421a9e6cee6aa3140e9d0a09d80fa9ad
ted.phillips <ted.phillips11@btinternet.com>
2022-02-22 11:37:31 +0000

    styling done for date and time selector

--- components/dnt-input/dnt-input-styled.ts
+++ components/dnt-input/dnt-input-styled.ts
@@ -2,6 +2,14 @@ import styled from "styled-components";
 import "antd/dist/antd.css";
 import { DatePicker, Space } from "antd";
 
-export const DnTInput = styled(DatePicker)``;
+export const DnTInput = styled(DatePicker)`
+  width: 100%;
+  height: 34.28px;
+`;
 
-export const SpaceComponent = styled(Space)``;
+export const SpaceComponent = styled(Space)`
+  position: relative;
+  top: -10px;
+  border: 2px solid black;
+  width: 100%;
+`;
