2abcea7a30035db76f0e12e6c4285e3621396758
ted.phillips <ted.phillips11@btinternet.com>
2022-02-22 16:08:25 +0000

    merging code

--- components/form/form-component.tsx
+++ components/form/form-component.tsx
@@ -3,11 +3,67 @@ import FieldComponent from "../field/field-component";
 import ListFieldComponent from "../list-field/list-field-component";
 import LongButtonComponent from "../long-button/long-button-component";
 import { Form } from "./form-styled";
+import { useReducer } from "react";
+import { useUser } from "@auth0/nextjs-auth0";
+import { userInfo } from "os";
+
+const { user } = useUser();
+
+const initialState = {
+  accepted_user_id: null,
+  date: null,
+  description: "",
+  rate_of_pay: null,
+  requirement: "",
+  status: "open",
+  title: "",
+  user_id: "",
+  user_image: user.picture,
+  user_name: user.name,
+  number: null,
+  name: "",
+  street: "",
+  city: "",
+  county: "",
+  postcode: "",
+  tags: [],
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "title":
+
+    case "number/name":
+
+    case "street":
+
+    case "town/city":
+
+    case "county":
+
+    case "postcode":
+
+    case "description":
+
+    case "tags":
+
+    case "requirements":
+
+    case "dateAndTime":
+
+    case "pay":
+
+    default:
+      return state;
+  }
+}
 
 function FormComponent() {
+  const [jobListing, dispatch] = useReducer(reducer, initialState);
+
   return (
     <Form>
-      <FieldComponent name="Job Title: *" />
+      <FieldComponent name="Job Title: *" onChange={() => dispatch()} />
       <FieldComponent name="Address Number/Name: *" />
       <FieldComponent name="Street: *" />
       <FieldComponent name="Town/City: *" />
