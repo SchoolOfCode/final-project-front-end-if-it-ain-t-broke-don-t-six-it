9e7d3cf0d7b0504673bd011f7897990842c93ee2
Guy-Robbins <guy.robbins1@gmail.com>
2022-02-22 15:50:55 +0000

    landing page component added

---
+++ components/search-box/search-box-styled.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const SearchBox = styled.div`
+background-color:black;
+top:-250px;
+border:solid 2px black;
+height:350px;
+width:90%;
+margin:auto;
+position:relative;
+`;
