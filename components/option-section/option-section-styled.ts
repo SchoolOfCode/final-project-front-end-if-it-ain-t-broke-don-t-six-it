bd9fce17d8680af999934977d1c06ec663c1a958
Rahrahrarara <92094786+Rahrahrarara@users.noreply.github.com>
2022-03-03 15:31:13 +0000

    option box shadow

--- components/option-section/option-section-styled.ts
+++ components/option-section/option-section-styled.ts
@@ -1,6 +1,12 @@
 import styled from "styled-components";
 
 export const OptionSection = styled.section`
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  margin: auto;
+  box-shadow: rgba(0, 0, 0, 0.35) 10px -2px 15px;
+
   .flex-container {
     display: flex;
     flex-direction: column;
