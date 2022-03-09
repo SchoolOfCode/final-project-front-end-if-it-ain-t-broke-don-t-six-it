b034b689fc9fbccf5b77dda9285ab94bbf3f8ede
Abdullahi Ali <ali259abdullahi@gmail.com>
2022-03-09 00:02:29 +0000

    tring to add favourits functionality

--- components/favourite-button/favourite-button-component.tsx
+++ components/favourite-button/favourite-button-component.tsx
@@ -3,15 +3,27 @@ import { useState } from "react";
 
 type Props = {
   isExtended: boolean;
+  isFavouriteToggle: boolean | undefined;
+  setIsFavouriteToggle: (e: boolean) => void;
+  isFavourited: boolean | undefined;
 };
 
-function FavouriteButtonComponent({ isExtended }: Props) {
+function FavouriteButtonComponent({
+  isExtended,
+  isFavouriteToggle,
+  setIsFavouriteToggle,
+  isFavourited,
+}: Props) {
   const [toggle, setToggle] = useState(false);
 
   return (
     <HeartImage
-      onClick={() => setToggle(!toggle)}
-      src={toggle ? "/heart-solid.svg" : "/heart-regular.svg"}
+      onClick={() => setIsFavouriteToggle(!isFavouriteToggle)}
+      src={
+        isFavouriteToggle || isFavourited
+          ? "/heart-solid.svg"
+          : "/heart-regular.svg"
+      }
       alt="favourite button"
       isExtended={isExtended}
     />
--- components/top-right/top-right-component.tsx
+++ components/top-right/top-right-component.tsx
@@ -4,12 +4,25 @@ import { TopRight } from "./top-right-styled";
 
 type Props = {
   source: string | undefined | null;
+  setIsFavouriteToggle: (e: boolean) => void;
+  isFavouriteToggle: boolean | undefined;
+  isFavourited: boolean | undefined;
 };
 
-function TopRightComponent({ source }: Props) {
+function TopRightComponent({
+  source,
+  setIsFavouriteToggle,
+  isFavouriteToggle,
+  isFavourited,
+}: Props) {
   return (
     <TopRight>
-      <FavouriteButtonComponent isExtended={false} />
+      <FavouriteButtonComponent
+        isExtended={false}
+        isFavouriteToggle={isFavouriteToggle}
+        setIsFavouriteToggle={setIsFavouriteToggle}
+        isFavourited={isFavourited}
+      />
       <UserImageComponent source={source} username="Sixit" />
     </TopRight>
   );
