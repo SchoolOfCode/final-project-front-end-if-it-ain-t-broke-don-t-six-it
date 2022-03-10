3d46c0ce5555a08262cedf8cc7265d8f987bcc6d
Abdullahi Ali <ali259abdullahi@gmail.com>
2022-03-10 11:10:40 +0000

    try to deploy

--- components/footer/footer-component.tsx
+++ components/footer/footer-component.tsx
@@ -4,8 +4,8 @@ function FooterComponent() {
   return (
     <Footer>
       <p>
-        Oddjob is a portfolio project piece created by 'If it ain't broke, don't
-        six it' All rights reserved.
+        {`Oddjob is a portfolio project piece created by 'If it ain't broke, don't
+        six it' All rights reserved.`}
       </p>
     </Footer>
   );
